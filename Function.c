a85ad2f3e5e1021550faabb20ba159c3b6c5cb29
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2014-05-24 17:56:31 +0800

    新增函式跟函式雛型的定義

---
+++ Function.c
@@ -0,0 +1,10 @@
+
+__RETURN_VALUE_DATA_TYPE __FUNCTION_NAME__(__ARGUMENT_TYPE__ __ARGUMENT_NAME__){
+	/* 變數宣告與定義
+	 * Variable declarations & definitions) */
+		
+	/* －－－－－－－－－－－－－－－－－－－－－ */
+		
+	/* －－－－－－－－－－－－－－－－－－－－－ */
+	return __RETURN_VALUE__;
+}
---
+++ Function_prototype.c
@@ -0,0 +1,3 @@
+/* Function description
+	 函式描述文字 */
+	__RETURN_VALUE_DATA_TYPE __FUNCTION_NAME__(__ARGUMENT_TYPE__ __ARGUMENT_NAME__);
