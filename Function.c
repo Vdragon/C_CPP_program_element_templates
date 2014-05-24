36888e3e0056321f88350015965ca650e14db531
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2014-05-24 18:10:44 +0800

    function - 新增 implementation 章節說明

--- Function.c
+++ Function.c
@@ -4,7 +4,9 @@ __RETURN_VALUE_DATA_TYPE __FUNCTION_NAME__(__ARGUMENT_TYPE__ __ARGUMENT_NAME__){
 	 * Variable declarations & definitions) */
 		
 	/* －－－－－－－－－－－－－－－－－－－－－ */
-		
+	/* 函式功能實作
+	   Implementation of a function's function */
+	
 	/* －－－－－－－－－－－－－－－－－－－－－ */
 	return __RETURN_VALUE__;
 }
